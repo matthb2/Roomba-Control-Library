f9a566eaf0fb9e7200c5497a242cef20c2825b1d
Ben Matthews <mben12@gmail.com>
2009-03-10 23:53:51 -0400

    Tweaked ramp speed in test code

--- utility/move.c
+++ utility/move.c
@@ -25,8 +25,8 @@ int main(int argc, char** argv)
 			case 'h':roomba_drive(fd,SPEED,CCLOCKWISE_INPLACE_RADIUS);
 				 break;
 			case 'k':roomba_drive(fd,SPEED,STRAIGHT_RADIUS);break;
-			case 'j':roomba_ramp(fd,0-SPEED,STRAIGHT_RADIUS,100);break;
-			case 'l':roomba_ramp(fd,SPEED,CLOCKWISE_INPLACE_RADIUS,100);break;
+			case 'j':roomba_ramp(fd,0-SPEED,STRAIGHT_RADIUS,50);break;
+			case 'l':roomba_ramp(fd,SPEED,CLOCKWISE_INPLACE_RADIUS,50);break;
 			case 's':roomba_ramp(fd,0,0,100);break;
 			case ERR:roomba_drive(fd,0,0);break;
 			case 'q':goto end;
